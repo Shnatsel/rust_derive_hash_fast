15389ea87575d3bd2a32e28c21a2293ec7b5598a
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2025-08-09 19:08:00 +0100

    Full implementation for bytemuck

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,59 @@
-use bytemuck::NoUninit;
+#[macro_export]
+macro_rules! derive_hash_fast_bytemuck {
+    ($T:ty) => {
+        impl core::hash::Hash for $T {
+            fn hash<H: core::hash::Hasher>(&self, state: &mut H) {
+                state.write(bytemuck::bytes_of(self));
+            }
 
-#[repr(C)]
-#[derive(Clone, Copy, NoUninit)]
-struct Foo {
-    a: u16,
-    b: u16,
-    c: char,
+            fn hash_slice<H: core::hash::Hasher>(data: &[Self], state: &mut H)
+            where
+                Self: Sized,
+            {
+                state.write(bytemuck::cast_slice(data));
+            }
+        }
+    };
 }
-
-use core::hash::{Hash, Hasher};
-impl Hash for Foo {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        state.write( bytemuck::bytes_of(self));
-    }
-
-    fn hash_slice<H: Hasher>(data: &[Self], state: &mut H)
-    where
-        Self: Sized,
-    {
-        state.write(bytemuck::cast_slice(data) );
-    }
-}
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use bytemuck::NoUninit;
+    use std::hash::{DefaultHasher, Hash, Hasher};
+
+    #[repr(C)]
+    #[derive(Clone, Copy, NoUninit)]
+    struct FooB {
+        a: u16,
+        b: u16,
+        c: char,
+    }
+
+    derive_hash_fast_bytemuck!(FooB);
+
+    fn hash_struct_bytemuck(val: FooB) -> u64 {
+        let mut hasher = DefaultHasher::new();
+        val.hash(&mut hasher);
+        hasher.finish()
+    }
 
     #[test]
-    fn it_works() {
-        // let result = add(2, 2);
-        // assert_eq!(result, 4);
+    fn test_bytemuck() {
+        let test_struct_1 = FooB {
+            a: 5,
+            b: 10,
+            c: 'a',
+        };
+        let test_struct_2 = FooB {
+            a: 5,
+            b: 500,
+            c: 'a',
+        };
+
+        let hash_1 = hash_struct_bytemuck(test_struct_1);
+        let hash_2 = hash_struct_bytemuck(test_struct_2);
+
+        assert_ne!(hash_1, hash_2);
     }
 }
