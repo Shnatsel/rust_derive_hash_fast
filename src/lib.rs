733269d1d0bb484082f9d82e72618ef2eb07716b
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2025-08-09 19:26:09 +0100

    Initial impl using zerocopy

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,33 @@ macro_rules! derive_hash_fast_bytemuck {
     ($T:ty) => {
         impl core::hash::Hash for $T {
             fn hash<H: core::hash::Hasher>(&self, state: &mut H) {
-                state.write(bytemuck::bytes_of(self));
+                state.write(::bytemuck::bytes_of(self));
             }
 
             fn hash_slice<H: core::hash::Hasher>(data: &[Self], state: &mut H)
             where
                 Self: Sized,
             {
-                state.write(bytemuck::cast_slice(data));
+                state.write(::bytemuck::cast_slice(data));
             }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! derive_hash_fast_zerocopy {
+    ($T:ty) => {
+        impl core::hash::Hash for $T {
+            fn hash<H: core::hash::Hasher>(&self, state: &mut H) {
+                state.write(::zerocopy::IntoBytes::as_bytes(self));
+            }
+
+            // fn hash_slice<H: core::hash::Hasher>(data: &[Self], state: &mut H)
+            // where
+            //     Self: Sized,
+            // {
+            //     state.write(bytemuck::cast_slice(data));
+            // }
         }
     };
 }
@@ -19,10 +37,9 @@ macro_rules! derive_hash_fast_bytemuck {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use bytemuck::NoUninit;
 
     #[repr(C)]
-    #[derive(Clone, Copy, NoUninit)]
+    #[derive(Clone, Copy, bytemuck::NoUninit)]
     struct FooB {
         a: u16,
         b: u16,
@@ -54,6 +71,44 @@ mod tests {
         let hash_1 = hash_struct_bytemuck(test_struct_1);
         let hash_1_again = hash_struct_bytemuck(test_struct_1);
         let hash_2 = hash_struct_bytemuck(test_struct_2);
+
+        assert_eq!(hash_1, hash_1_again);
+        assert_ne!(hash_1, hash_2);
+    }
+
+    #[repr(C)]
+    #[derive(Clone, Copy, zerocopy::Immutable, zerocopy::IntoBytes)]
+    struct FooZ {
+        a: u16,
+        b: u16,
+        c: char,
+    }
+
+    derive_hash_fast_zerocopy!(FooZ);
+
+    fn hash_struct_zerocopy(val: FooZ) -> u64 {
+        use std::hash::{DefaultHasher, Hash, Hasher};
+        let mut hasher = DefaultHasher::new();
+        val.hash(&mut hasher);
+        hasher.finish()
+    }
+
+    #[test]
+    fn test_zerocopy() {
+        let test_struct_1 = FooZ {
+            a: 5,
+            b: 10,
+            c: 'a',
+        };
+        let test_struct_2 = FooZ {
+            a: 5,
+            b: 500,
+            c: 'a',
+        };
+
+        let hash_1 = hash_struct_zerocopy(test_struct_1);
+        let hash_1_again = hash_struct_zerocopy(test_struct_1);
+        let hash_2 = hash_struct_zerocopy(test_struct_2);
 
         assert_eq!(hash_1, hash_1_again);
         assert_ne!(hash_1, hash_2);
